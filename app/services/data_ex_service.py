e7bbf223b6bcdcae04228632090dd74c94fa3cba
itdragons <itdragons@qq.com>
2026-03-29 22:03:22 +0800

    download_history_data

--- app/services/data_ex_service.py
+++ app/services/data_ex_service.py
@@ -43,13 +43,17 @@ class DataExService:
         # 先下载历史数据（确保本地有数据）                        
         if not request.disable_download:
             logger.debug("下载历史数据...")
+            n = 1
+            num = len(request.stock_list)
             for stock_code in request.stock_list:
+                print(f"当前正在下载 {request.period} {n}/{num}")
                 xtdata.download_history_data(
                     stock_code=stock_code,
                     period=request.period,
                     start_time=request.start_time,
                     end_time=request.end_time
                 )
+                n += 1
 
     def get_market_data_ex(self, request: MarketDataExRequest) -> Dict[str, Dict[str, List]]:
         """获取历史行情与实时行情（xtdata.get_market_data_ex）
